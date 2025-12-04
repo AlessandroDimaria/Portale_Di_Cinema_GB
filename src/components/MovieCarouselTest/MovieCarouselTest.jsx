8c9c279f5289eea51fe6f021ba6ee54523aa4c87
Damilola91 <damilola.wiwoloku@gmail.com>
2025-12-04 14:44:08 +0100

    modifica context e components

--- src/components/MovieCarouselTest/MovieCarouselTest.jsx
+++ src/components/MovieCarouselTest/MovieCarouselTest.jsx
@@ -1,39 +1,25 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-import MovieCardTest from "../MovieCardTest/MovieCardTest";
-import { useMoviesApi } from "../../context/MoviesContext";
 
+import MovieCardTest from "../MovieCardTest/MovieCardTest";
 import "swiper/css";
 import "swiper/css/navigation";
 
-const MovieCarousel = ({ section, onSelectMovie }) => {
-  const { home, homeLoading, homeError } = useMoviesApi();
-
-  if (homeLoading) return <p className="status-text">Caricamento…</p>;
-  if (homeError)
-    return <p className="status-text status-text--error">{homeError}</p>;
-
-  const items = home[section.key]?.results || [];
+const MovieCarouselTest = ({ title, movies = [], onSelectMovie }) => {
+  if (!movies) return null;
 
   return (
     <section className="carousel-section">
-      <h2 className="carousel-title">{section.title}</h2>
+      <h2 className="carousel-title">{title}</h2>
 
       <Swiper
         modules={[Navigation]}
         navigation
-        spaceBetween={35}
-        slidesPerView={5}
-        breakpoints={{
-          320: { slidesPerView: 2 },
-          480: { slidesPerView: 3 },
-          768: { slidesPerView: 4 },
-          1024: { slidesPerView: 6 },
-          1400: { slidesPerView: 7 },
-        }}
+        slidesPerView={6}
+        spaceBetween={20}
         className="movie-swiper"
       >
-        {items.map((movie) => (
+        {movies.map((movie) => (
           <SwiperSlide key={movie.id}>
             <MovieCardTest
               movie={movie}
@@ -46,4 +32,4 @@ const MovieCarousel = ({ section, onSelectMovie }) => {
   );
 };
 
-export default MovieCarousel;
+export default MovieCarouselTest;
