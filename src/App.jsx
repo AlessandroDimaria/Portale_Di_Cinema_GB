4a9c264c28b362aeb89ca9656b7a45f0a02dfdec
AlessandroDimaria <arasmas3@gmail.com>
2025-12-04 14:58:59 +0100

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import MoviesCarouselTest from "./components/MovieCarouselTest/MovieCarouselTest";
 import MovieApp from "./components/MovieApp/MovieApp";
 import "./App.css";
+import Homepage from "./components/MovieHomepage/Homepage"; 
 
 const App = () => {
   return (
     <>
+  
       <MovieApp />
+       <div className="app">
+      <Homepage />
+    </div>
     </>
   );
 };
---
+++ src/components/MovieHomepage/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>🔍 </li>
+        <li>🏠 </li>
+        <li>🎬 </li>
+        <li>⚙️ </li>
+      </ul>
+    </div>
+  );
+}
