8dc3d36411b09dbc8bd79e57b3f8d2761e67ac44
Giuseppe Sica <92546622+PepSic97@users.noreply.github.com>
2025-12-05 15:16:16 +0100

    Smistamento css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import MovieApp from "./components/MovieApp/MovieApp";
 import "./App.css";
+import "./styles/navBar.css";
+import "./styles/search.css";
+import "./styles/swipe.css";
+import "./styles/details.css";
+import "./styles/movieCard.css";
+import "./styles/status.css";
+
 import MovieList from "./components/Testvideo/TestVideo";
 const App = () => {
   return (
