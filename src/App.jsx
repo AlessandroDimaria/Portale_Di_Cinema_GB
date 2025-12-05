0507e881666c432c0599e0c2bcdfabadf2b126d4
Giuseppe Sica <92546622+PepSic97@users.noreply.github.com>
2025-12-05 15:22:26 +0100

    Merge media da develop e smistato nel suo file, chiamato in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "./styles/swipe.css";
 import "./styles/details.css";
 import "./styles/movieCard.css";
 import "./styles/status.css";
-
+import "./styles/media.css";
 import MovieList from "./components/Testvideo/TestVideo";
 const App = () => {
   return (
