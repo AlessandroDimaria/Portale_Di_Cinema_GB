f5537df10ddd88921d4be77b744228f1cb26008d
AlessandroDimaria <arasmas3@gmail.com>
2025-12-05 15:14:01 +0100

    resposivness

--- src/components/MovieCarouselTest/MovieCarouselTest.jsx
+++ src/components/MovieCarouselTest/MovieCarouselTest.jsx
@@ -12,22 +12,28 @@ const MovieCarouselTest = ({ title, movies = [], onSelectMovie }) => {
     <section className="carousel-section">
       <h2 className="carousel-title">{title}</h2>
 
-      <Swiper
-        modules={[Navigation]}
-        navigation
-        slidesPerView={6}
-        spaceBetween={20}
-        className="movie-swiper"
-      >
-        {movies.map((movie) => (
-          <SwiperSlide key={movie.id}>
-            <MovieCardTest
-              movie={movie}
-              onClick={() => onSelectMovie(movie.id)}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+<Swiper
+  modules={[Navigation]}
+  navigation
+  spaceBetween={20}
+  breakpoints={{
+    320: { slidesPerView: 2, spaceBetween: 10 },
+    576: { slidesPerView: 3, spaceBetween: 15 },
+    768: { slidesPerView: 4, spaceBetween: 20 },
+    992: { slidesPerView: 5, spaceBetween: 25 },
+    1200: { slidesPerView: 6, spaceBetween: 30 },
+  }}
+  className="movie-swiper"
+>
+  {movies.map((movie) => (
+    <SwiperSlide key={movie.id}>
+      <MovieCardTest
+        movie={movie}
+        onClick={() => onSelectMovie(movie.id)}
+      />
+    </SwiperSlide>
+  ))}
+</Swiper>
     </section>
   );
 };
